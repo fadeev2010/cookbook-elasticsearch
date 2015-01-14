63950e6857790cfa17d5131d93ba4c2b11a966ca
fadeev2010 <fadeev2010@gmail.com>
2015-01-14 05:03:33 +0300

    fix attr

--- libraries/print_value.rb
+++ libraries/print_value.rb
@@ -7,19 +7,19 @@ module Extensions
     #
     # 1. Using the key as a node attribute:
     #
-    #    <%= print_value 'bar' -%> is evaluated as: `node.elasticsearch[:bar]`
+    #    <%= print_value 'bar' -%> is evaluated as: `node.elasticsearch142[:bar]`
     #
     #    You may use a dot-separated key for nested attributes:
     #
     #    <%= print_value 'foo.bar' -%> is evaluated in multiple ways in this order:
     #
-    #    a) as `node.elasticsearch['foo.bar']`,
-    #    b) as `node.elasticsearch['foo_bar']`,
-    #    c) as `node.elasticsearch.foo.bar` (ie. `node.elasticsearch[:foo][:bar]`)
+    #    a) as `node.elasticsearch142['foo.bar']`,
+    #    b) as `node.elasticsearch142['foo_bar']`,
+    #    c) as `node.elasticsearch142.foo.bar` (ie. `node.elasticsearch142[:foo][:bar]`)
     #
     # 2. You may also provide an explicit value for the method, which is then used:
     #
-    #    <%= print_value 'bar', node.elasticsearch[:foo] -%>
+    #    <%= print_value 'bar', node.elasticsearch142[:foo] -%>
     #
     # You may pass a specific separator to the method:
     #
@@ -32,9 +32,9 @@ module Extensions
       existing_value   = value
 
       # NOTE: A value of `false` is valid, we need to check for `nil` explicitely
-      existing_value = node.elasticsearch[key] if existing_value.nil? and not node.elasticsearch[key].nil?
-      existing_value = node.elasticsearch[key.tr('.', '_')] if existing_value.nil? and not node.elasticsearch[key.tr('.', '_')].nil?
-      existing_value = key.to_s.split('.').inject(node.elasticsearch) { |result, attr| result[attr] } rescue nil if existing_value.nil?
+      existing_value = node.elasticsearch142[key] if existing_value.nil? and not node.elasticsearch142[key].nil?
+      existing_value = node.elasticsearch142[key.tr('.', '_')] if existing_value.nil? and not node.elasticsearch142[key.tr('.', '_')].nil?
+      existing_value = key.to_s.split('.').inject(node.elasticsearch142) { |result, attr| result[attr] } rescue nil if existing_value.nil?
 
       [key, separator, existing_value.to_s, "\n"].join unless existing_value.nil?
     end
